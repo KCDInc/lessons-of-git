f2f62c7251de9bbc907f83339d0dec478aa6e2ad
Alex Andreevich <kcdcloud@outlook.com>
2018-08-12 21:08:45 +0300

    img + section 2.4

--- git-config.sh
+++ git-config.sh
@@ -1,3 +1,9 @@
+: << COMMENTBLOCK
+1. Файл /etc/gitconfig содержит значения, общие для всех пользователей системы и для всех их репозиториев. Если при запуске git config указать параметр --system, то параметры будут читаться и сохраняться именно в этот файл.
+2. Файл ~/.gitconfig или ~/.config/git/config хранит настройки конкретного пользователя. Этот файл используется при указании параметра --global.
+3. Файл config в каталоге Git’а (т.е. .git/config) в том репозитории, который вы используете в данный момент, хранит настройки конкретного репозитория.
+COMMENTBLOCK
+echo "Код завершения "COMMENTBLOCK" = $?." # 0
 # name un --global set for curent folder
 git config --global user.name "Alex Andreevich"
 git config --global user.email "kcdcloud@outlook.com"
